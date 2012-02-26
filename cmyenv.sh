79b86d5d16f8b0884cdb53716ad8236f5cff3959
Jasmeet Chhabra <jasmeet@chhabra-inc.com>
2012-02-25 16:29:42 -0800

    Initial version updated

---
+++ cmyenv.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+EXPECTED_ARGS=1
+if [ $# -ge "$EXPECTED_ARGS" ]; then
+	cd $1
+fi
+
+
+if [ -e "./vim" ]; then
+	echo "Setting up your environment"
+else
+	echo "Sorry. Please run the command from the directory it is in, or provide the basedir. as argument"
+	exit 1
+fi
+ln -s $PWD/vim ~/.vim
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/gvimrc ~/.gvimrc
+ln -s $PWD/rapp/rapp ~/bin/rapp
+ln -s $PWD/rapp/rapp-files ~/bin/rapp-files
